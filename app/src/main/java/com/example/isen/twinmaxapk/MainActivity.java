16072cda706f8152a1cfb58c8db64d54df4dcdfe
ThomBrice <thomas.brice.59@gmail.com>
2016-03-17 17:09:08 +0100

    Graphiques et ActiviyMain

--- app/src/main/java/com/example/isen/twinmaxapk/MainActivity.java
+++ app/src/main/java/com/example/isen/twinmaxapk/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.isen.twinmaxapk;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b0 = (Button) findViewById(R.id.b0);
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Acquisition.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
