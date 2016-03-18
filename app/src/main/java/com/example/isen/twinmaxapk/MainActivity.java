c2935651b4c98cf6a6bb41911d5d3c8d25ac54b4
Tbtimber <delpierre.matthieu@gmail.com>
2016-03-18 13:20:27 +0100

    Merge branch 'master' of https://github.com/ThomBrice/TwinmaxApk into BLE_parser
    # Conflicts:
    #	.idea/libraries/animated_vector_drawable_23_2_1.xml
    #	.idea/libraries/appcompat_v7_23_2_1.xml
    #	.idea/libraries/support_annotations_23_2_1.xml
    #	.idea/libraries/support_v4_23_2_1.xml
    #	.idea/libraries/support_vector_drawable_23_2_1.xml
    #	.idea/workspace.xml
    #	app/build.gradle
    #	app/src/main/java/com/example/isen/twinmaxapk/MainActivity.java

--- app/src/main/java/com/example/isen/twinmaxapk/MainActivity.java
+++ app/src/main/java/com/example/isen/twinmaxapk/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.isen.twinmaxapk;
 
 import android.app.Activity;
+<<<<<<< HEAD
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +11,27 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
 
+=======
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+>>>>>>> 2ffb67e315bf214e421518cfca842772d8d0d9d9
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b0 = (Button) findViewById(R.id.b0);
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Acquisition.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
